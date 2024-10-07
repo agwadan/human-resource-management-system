031c23c23b930b4952cfd37a79d240606521e884
agwadan <agwa.dan@gmail.com>
2024-10-07 10:31:02 +0300

    Style page to add staff

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav style={styles.navbar}>
+      <ul style={styles.navList}>
+        <li style={styles.navItem}>
+          <Link to="/" style={styles.navLink}>
+            Register Staff
+          </Link>
+        </li>
+        <li style={styles.navItem}>
+          <Link to="/retrieve" style={styles.navLink}>
+            Retrieve Staff
+          </Link>
+        </li>
+        <li style={styles.navItem}>
+          <Link to="/update" style={styles.navLink}>
+            Update Staff
+          </Link>
+        </li>
+        <li style={styles.navItem}>
+          <Link to="/login-admin" style={styles.navLink}>
+            Admin Dashboard
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+// Simple styles for the navbar
+const styles = {
+  navbar: {
+    backgroundColor: "#282c34",
+    padding: "10px",
+  },
+  navList: {
+    listStyleType: "none",
+    display: "flex",
+    justifyContent: "space-around",
+    margin: 0,
+    padding: 0,
+  },
+  navItem: {
+    margin: "0 15px",
+  },
+  navLink: {
+    color: "white",
+    textDecoration: "none",
+    fontSize: "16px",
+  },
+};
+
+export default Navbar;
